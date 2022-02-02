32e03f57ed7a841a6774ff6d3d98a5946b934f0c
zack zheng <zack_zhengxiyun@163.com>
2022-02-02 10:44:51 +0800

    no message

--- known/node/codes/demo1/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const nameList = fs.readdirSync(__dirname)
-
-nameList.forEach(currFileName => {
-  if (currFileName.endsWith('.js')) {
-    fs.renameSync(currFileName, `zack${currFileName}`)
-  }
-})
