9518b31ef5a6b6d009105a08c5729b6bc8ed3897
zack_zheng <zack_zhengxiyun@163.com>
2022-06-04 10:31:44 +0800

    hello-react

--- known/react/learn-demo/src/components/my-rc-field-form/Form.js
+++ known/react/learn-demo/src/components/my-rc-field-form/Form.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import FieldContext from './FieldContext'
+import useForm from './useForm'
 
 export default function Form({children}) {
+  const {formInstance} = useForm()
   return (
-    <form>
+    <form onSubmit={event=>{
+      event.preventDefault()
+    }}>
+    <FieldContext.Provider value={formInstance}>
       {children}
+    </FieldContext.Provider>
     </form>
   )
 }
--- known/react/learn-demo/src/components/my-rc-field-form/useForm.js
+++ known/react/learn-demo/src/components/my-rc-field-form/useForm.js
@@ -1,3 +1,26 @@
-export default function useForm() {
-  return []
+import React, {useRef} from 'react'
+
+class FormStore {
+  constructor() {
+    this.store = {} // 存储数据
+  }
+
+  setFieldsValue = () => {}
+
+  getForm() {
+    return {
+      setFieldsValue: this.setFieldsValue
+    }
+  }
+}
+
+// 自定义hook
+export default function useForm(props) {
+  const formRef = useRef()
+  if(!formRef.current) {
+    // new一个
+    const formStore = new FormStore()
+    formRef.current = formStore
+  }
+  return [formRef.current]
 }
