2b1fdd51b1eeecbe34448c69f21009bba7237827
zack_zheng <zack_zhengxiyun@163.com>
2022-06-05 21:32:55 +0800

    no message

---
+++ known/react/hello-react/src/pages/Clock.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Clock extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
