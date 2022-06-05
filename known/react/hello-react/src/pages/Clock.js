6bd96a4c958d3758b8a4fe5b8861c003d29af0c3
zack_zheng <zack_zhengxiyun@163.com>
2022-06-05 22:20:50 +0800

    no message

--- known/react/hello-react/src/pages/Clock.js
+++ known/react/hello-react/src/pages/Clock.js
@@ -9,6 +9,20 @@ export default class Clock extends Component {
     }
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
   render() {
     return (
       <div>
