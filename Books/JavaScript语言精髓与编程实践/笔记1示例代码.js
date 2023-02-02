e2185bb7f87b37e5ce7d7c2179eda6913042f406
zack zheng <zack_zhengxiyun@163.com>
2023-02-02 17:50:49 +0800

    book record

---
+++ Books/JavaScript语言精髓与编程实践/笔记1示例代码.js
@@ -0,0 +1,19 @@
+var str = 'abcde'
+var obj = new String(str)
+
+function newToString() {
+  return 'hello world'
+}
+
+function func(val) {
+  val.toString = newToString
+}
+
+
+// 示例1:传入值
+func(str)
+console.log(str)  // 'abcde'
+
+// 示例2:传入引用
+func(obj)
+console.log(String(obj)) // 'hello world'
