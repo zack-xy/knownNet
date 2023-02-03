8825bd3fa8b36afa6e3d915f46c75918208acd62
zack zheng <zack_zhengxiyun@163.com>
2023-02-03 17:47:38 +0800

    no message

--- Books/JavaScript语言精髓与编程实践/笔记1示例代码.js
+++ Books/JavaScript语言精髓与编程实践/笔记1示例代码.js
@@ -17,3 +17,23 @@ console.log(str)  // 'abcde'
 // 示例2:传入引用
 func(obj)
 console.log(String(obj)) // 'hello world'
+
+
+
+
+// 每行结尾使用\
+var aTextBlock = '\
+adasdsasadsadsadasdsadsa\
+ \
+123456789\
+ \
++-*/'
+
+
+// 模板字符串，避免换行符，每行末尾使用\
+var text2 = `3ewqeqe\
+weqwewq\
+1233445\
+wqewqe\
+ewqeqw\
+ewqewq`
