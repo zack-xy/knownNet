1abdb9d11e82a0d09b3c45bc979556f70cf5df64
zack zheng <zack_zhengxiyun@163.com>
2023-02-16 17:01:20 +0800

    remove some codes

--- Program/react/learn-demo/src/pages/Context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// 通过context传递参数
-/**
- * 1.创建context
- * 2.创建Provider
- */
-export const ThemeConext = React.createContext()
-export const ThemeProvider = ThemeConext.Provider;
-export const ThemeConsumer = ThemeConext.Consumer
