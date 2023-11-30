ce5b4505716126c46684e8e3a20fe50f6e080efa
zack-xy <zack_zhengxiyun@163.com>
2023-11-30 13:26:35 +0000

    Deploying to gh-pages from @ zack-xy/knownNet@51dd6ec4e0152589bbef00f39e646ed3698851c8 🚀

---
+++ assets/program_pieces_2021_02_html特殊标签和特殊符号.md.15e5d88c.js
@@ -0,0 +1,91 @@
+import{_ as s,c as a,o as n,a as l}from"./app.ce46b49b.js";const C=JSON.parse('{"title":"html特殊标签和特殊符号","description":"","frontmatter":{"title":"html特殊标签和特殊符号","author":"Zack Zheng","date":"2021/02/05 22:36","categories":["大海拾遗"],"tags":["Html"]},"headers":[],"relativePath":"program/pieces/2021/02/html特殊标签和特殊符号.md","lastUpdated":1676529397000}'),p={name:"program/pieces/2021/02/html特殊标签和特殊符号.md"},e=l(`<hr><h4 id="符集介绍" tabindex="-1">符集介绍 <a class="header-anchor" href="#符集介绍" aria-hidden="true">#</a></h4><p>gb2312 国标6763个汉字 BIG5 繁体中文 GBK 全部中文，国标扩展 gb2312扩展包含繁体 UTF-8基本包含全世界所有国家包含的字符</p><hr><h4 id="横线、换行、加粗、删除线、下划线标签" tabindex="-1">横线、换行、加粗、删除线、下划线标签 <a class="header-anchor" href="#横线、换行、加粗、删除线、下划线标签" aria-hidden="true">#</a></h4><div class="language- line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight vp-code-dark"><code><span class="line"><span style="color:#A6ACCD;">&lt;hr /&gt;   </span></span>
+<span class="line"><span style="color:#A6ACCD;">横线</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;br /&gt;   </span></span>
+<span class="line"><span style="color:#A6ACCD;">换行</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;b&gt;&lt;/b&gt;&lt;strong&gt;&lt;/strong&gt;   </span></span>
+<span class="line"><span style="color:#A6ACCD;">加粗   (XHTML都推荐用后面的)</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;i&gt;&lt;/i&gt; &lt;em&gt;&lt;/em&gt;                </span></span>
+<span class="line"><span style="color:#A6ACCD;">倾斜</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;s&gt;&lt;/s&gt;&lt;del&gt;&lt;/del&gt;               </span></span>
+<span class="line"><span style="color:#A6ACCD;">删除线</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;u&gt;&lt;/u&gt;&lt;ins&gt;&lt;/ins&gt;               </span></span>
+<span class="line"><span style="color:#A6ACCD;">下划线</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;a href=&quot;#&quot;&gt;&lt;/a&gt;                     </span></span>
+<span class="line"><span style="color:#A6ACCD;">空链接  target=&quot;_blank&quot; 新窗口</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;base target=&quot;_blank&quot;/&gt;  </span></span>
+<span class="line"><span style="color:#A6ACCD;">写在&lt;head&gt;&lt;/head&gt;中，使全体链接都新窗口打开</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><pre class="shiki one-dark-pro vp-code-light"><code><span class="line"><span style="color:#abb2bf;">&lt;hr /&gt;   </span></span>
+<span class="line"><span style="color:#abb2bf;">横线</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;br /&gt;   </span></span>
+<span class="line"><span style="color:#abb2bf;">换行</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;b&gt;&lt;/b&gt;&lt;strong&gt;&lt;/strong&gt;   </span></span>
+<span class="line"><span style="color:#abb2bf;">加粗   (XHTML都推荐用后面的)</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;i&gt;&lt;/i&gt; &lt;em&gt;&lt;/em&gt;                </span></span>
+<span class="line"><span style="color:#abb2bf;">倾斜</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;s&gt;&lt;/s&gt;&lt;del&gt;&lt;/del&gt;               </span></span>
+<span class="line"><span style="color:#abb2bf;">删除线</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;u&gt;&lt;/u&gt;&lt;ins&gt;&lt;/ins&gt;               </span></span>
+<span class="line"><span style="color:#abb2bf;">下划线</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;a href=&quot;#&quot;&gt;&lt;/a&gt;                     </span></span>
+<span class="line"><span style="color:#abb2bf;">空链接  target=&quot;_blank&quot; 新窗口</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;base target=&quot;_blank&quot;/&gt;  </span></span>
+<span class="line"><span style="color:#abb2bf;">写在&lt;head&gt;&lt;/head&gt;中，使全体链接都新窗口打开</span></span>
+<span class="line"><span style="color:#abb2bf;"></span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br></div></div><hr><h4 id="锚点定位" tabindex="-1">锚点定位 <a class="header-anchor" href="#锚点定位" aria-hidden="true">#</a></h4><div class="language- line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight vp-code-dark"><code><span class="line"><span style="color:#A6ACCD;">1、使用相应的id名标注跳转目标的位置</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;div id=&quot;zackTest&quot;&gt;网页内容&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">2、使用&lt;a href=&quot;#zackTest&quot;&gt;锚点链接&lt;/a&gt;  </span></span>
+<span class="line"><span style="color:#A6ACCD;">href指向id</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><pre class="shiki one-dark-pro vp-code-light"><code><span class="line"><span style="color:#abb2bf;">1、使用相应的id名标注跳转目标的位置</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;div id=&quot;zackTest&quot;&gt;网页内容&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#abb2bf;"></span></span>
+<span class="line"><span style="color:#abb2bf;">2、使用&lt;a href=&quot;#zackTest&quot;&gt;锚点链接&lt;/a&gt;  </span></span>
+<span class="line"><span style="color:#abb2bf;">href指向id</span></span>
+<span class="line"><span style="color:#abb2bf;"></span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br></div></div><hr><h4 id="特殊符号" tabindex="-1">特殊符号 <a class="header-anchor" href="#特殊符号" aria-hidden="true">#</a></h4><div class="language- line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight vp-code-dark"><code><span class="line"><span style="color:#A6ACCD;">不换行空格：&amp;nbsp; （空白键）</span></span>
+<span class="line"><span style="color:#A6ACCD;">半角空格：&amp;ensp;    （二分之一em宽）</span></span>
+<span class="line"><span style="color:#A6ACCD;">全角空格：&amp;emsp;    （一个em宽）</span></span>
+<span class="line"><span style="color:#A6ACCD;">窄空格： &amp;thinsp;      （六分之一em宽）</span></span>
+<span class="line"><span style="color:#A6ACCD;">零宽不连字：&amp;zwnj;（不打印字符，抑制连字）</span></span>
+<span class="line"><span style="color:#A6ACCD;">零宽连字：&amp;zwj;      (不打印字符，使字相连)</span></span>
+<span class="line"><span style="color:#A6ACCD;">其他空白：</span></span>
+<span class="line"><span style="color:#A6ACCD;">空格（&amp;#x0020;）、</span></span>
+<span class="line"><span style="color:#A6ACCD;">制表位（&amp;#x0009;）、</span></span>
+<span class="line"><span style="color:#A6ACCD;">换行（&amp;#x000A;）</span></span>
+<span class="line"><span style="color:#A6ACCD;">回车（&amp;#x000D;）</span></span>
+<span class="line"><span style="color:#A6ACCD;">（&amp;#12288;）</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;小于号：&amp;lt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&gt;大于号：&amp;gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&amp;和号：&amp;amp;</span></span>
+<span class="line"><span style="color:#A6ACCD;">¥人民币：&amp;yen;</span></span>
+<span class="line"><span style="color:#A6ACCD;">©版权：&amp;copy;</span></span>
+<span class="line"><span style="color:#A6ACCD;">®注册商标：&amp;reg;</span></span>
+<span class="line"><span style="color:#A6ACCD;">。摄氏度: &amp;deg;</span></span>
+<span class="line"><span style="color:#A6ACCD;">±正负号：&amp;plusmn;</span></span>
+<span class="line"><span style="color:#A6ACCD;">x乘号：&amp;times;</span></span>
+<span class="line"><span style="color:#A6ACCD;">➗除号：&amp;divide;</span></span>
+<span class="line"><span style="color:#A6ACCD;">2 平方2（上标2）：&amp;sup2;</span></span>
+<span class="line"><span style="color:#A6ACCD;">3 立方3（上标3）：&amp;sup3;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><pre class="shiki one-dark-pro vp-code-light"><code><span class="line"><span style="color:#abb2bf;">不换行空格：&amp;nbsp; （空白键）</span></span>
+<span class="line"><span style="color:#abb2bf;">半角空格：&amp;ensp;    （二分之一em宽）</span></span>
+<span class="line"><span style="color:#abb2bf;">全角空格：&amp;emsp;    （一个em宽）</span></span>
+<span class="line"><span style="color:#abb2bf;">窄空格： &amp;thinsp;      （六分之一em宽）</span></span>
+<span class="line"><span style="color:#abb2bf;">零宽不连字：&amp;zwnj;（不打印字符，抑制连字）</span></span>
+<span class="line"><span style="color:#abb2bf;">零宽连字：&amp;zwj;      (不打印字符，使字相连)</span></span>
+<span class="line"><span style="color:#abb2bf;">其他空白：</span></span>
+<span class="line"><span style="color:#abb2bf;">空格（&amp;#x0020;）、</span></span>
+<span class="line"><span style="color:#abb2bf;">制表位（&amp;#x0009;）、</span></span>
+<span class="line"><span style="color:#abb2bf;">换行（&amp;#x000A;）</span></span>
+<span class="line"><span style="color:#abb2bf;">回车（&amp;#x000D;）</span></span>
+<span class="line"><span style="color:#abb2bf;">（&amp;#12288;）</span></span>
+<span class="line"><span style="color:#abb2bf;">&lt;小于号：&amp;lt;</span></span>
+<span class="line"><span style="color:#abb2bf;">&gt;大于号：&amp;gt;</span></span>
+<span class="line"><span style="color:#abb2bf;">&amp;和号：&amp;amp;</span></span>
+<span class="line"><span style="color:#abb2bf;">¥人民币：&amp;yen;</span></span>
+<span class="line"><span style="color:#abb2bf;">©版权：&amp;copy;</span></span>
+<span class="line"><span style="color:#abb2bf;">®注册商标：&amp;reg;</span></span>
+<span class="line"><span style="color:#abb2bf;">。摄氏度: &amp;deg;</span></span>
+<span class="line"><span style="color:#abb2bf;">±正负号：&amp;plusmn;</span></span>
+<span class="line"><span style="color:#abb2bf;">x乘号：&amp;times;</span></span>
+<span class="line"><span style="color:#abb2bf;">➗除号：&amp;divide;</span></span>
+<span class="line"><span style="color:#abb2bf;">2 平方2（上标2）：&amp;sup2;</span></span>
+<span class="line"><span style="color:#abb2bf;">3 立方3（上标3）：&amp;sup3;</span></span>
+<span class="line"><span style="color:#abb2bf;"></span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br></div></div>`,12),t=[e];function c(r,o,b,i,m,d){return n(),a("div",null,t)}const u=s(p,[["render",c]]);export{C as __pageData,u as default};
