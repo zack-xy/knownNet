9a87f2b86c56b8bc0095efd8455d8ac7ef41b1df
zack-xy <zack_zhengxiyun@163.com>
2025-07-03 08:23:23 +0000

    Deploying to gh-pages from @ zack-xy/knownNet@85ab81a380988c1cd0c4a08af8d31956cd7540cc 🚀

---
+++ assets/books_socialSciences_index.md.-y5khixU.lean.js
@@ -0,0 +1,7 @@
+import{b as i,C as d}from"./chunks/theme.DiUeXh3q.js";/* empty css                     */import{_ as r,c,o as _,aG as p,G as t,w as o,a as n,j as a}from"./chunks/framework.CdnsPyyz.js";const C=JSON.parse('{"title":"","description":"","frontmatter":{"showArticleMetadata":false,"editLink":false,"lastUpdated":false,"showComment":false},"headers":[],"relativePath":"books/socialSciences/index.md","filePath":"books/socialSciences/index.md"}'),u={name:"books/socialSciences/index.md"};function m(f,e,x,b,k,h){const l=i,s=d;return _(),c("div",null,[e[3]||(e[3]=p("",3)),t(l,null,{default:o(()=>e[0]||(e[0]=[n("没看完或者没有笔记的书")])),_:1}),t(s,{title:"《脑袋决定钱袋》"},{default:o(()=>e[1]||(e[1]=[a("div",null,[n(" 读了19页，鸡汤文学鉴定完毕，读完了也是那么回事，不读了 （看来是我想发财的心情非常急迫了，什么时候下载的这本书都不知道） "),a("pre",null,[a("code",null,`--------- 2025/04/03 11:32
+`)])],-1)])),_:1}),t(s,{title:"《一想到还有95%的问题留给人类我就放心了》"},{default:o(()=>e[2]||(e[2]=[a("div",null,[n(" 这个，怎么说呢，笔记不好记啊，基本上都是物理内容 "),a("pre",null,[a("code",null,`记录下进度吧   
+
+25% 2025年4月10日 
+
+2025年04月14日 基本读完，剩了三章，不读了，科普读物，就这样了
+`)])],-1)])),_:1})])}const N=r(u,[["render",m]]);export{C as __pageData,N as default};
