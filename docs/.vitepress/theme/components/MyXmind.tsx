78cc0fa7ad911e28101ebd1a7544616cdad18979
zack zheng <zack_zhengxiyun@163.com>
2025-07-10 15:01:33 +0800

    docs: update

--- docs/.vitepress/theme/components/MyXmind.tsx
+++ docs/.vitepress/theme/components/MyXmind.tsx
@@ -3,42 +3,34 @@ import { XMindEmbedViewer } from "xmind-embed-viewer";
 export default defineComponent({
   setup() {
 
+    function base64ToArrayBuffer(base64: string) {
+      const binaryStr = atob(base64); // 解码 base64 成二进制字符串
+      const len = binaryStr.length;
+      const bytes = new Uint8Array(len);
+
+      for (let i = 0; i < len; i++) {
+        bytes[i] = binaryStr.charCodeAt(i);
+      }
+
+      return bytes.buffer; // 返回 ArrayBuffer
+    }
+
     onMounted(() => {
       const viewer = new XMindEmbedViewer({
         el: '#xmind-viewer',
         region: 'cn'
       })
 
-      // 跨域了不支持
-
-      // fetch('https://gitee.com/api/v5/repos/zackzhengxy/xmind/contents/src/Grid属性.xmind')
-      //   .then(res => res.json())
-      //   .then(data => {
-      //     const downloadUrl = data.download_url;
-
-      //     // 第二步：用 download_url 获取真正的 .xmind 二进制文件
-      //     return fetch(downloadUrl)
-      //       .then(res => res.arrayBuffer());
-      //   })
-      //   .then(file => {
-      //     viewer.load(file); // ✅ 加载脑图
-      //   })
-      //   .catch(err => {
-      //     console.error('加载失败：', err);
-      //   });
-
-
-      // 效果不太行，错乱了
-      fetch('/knownNet/xmind/Grid属性.xmind') // 相对路径即可
-        .then(res => res.arrayBuffer())
-        .then(file => {
-          viewer.load(file)
-          viewer.setZoomScale(100)
-          viewer.setFitMap()
-          viewer.setStyles({
-            'width': '100%',
-            // CSS styles are available here
-          })
+      // 会被 403 Forbidden (Rate Limit Exceeded)
+      fetch('https://gitee.com/api/v5/repos/zackzhengxy/xmind/contents/src/Grid属性.xmind?access_token=???')
+        .then(res => res.json())
+        .then(data => {
+          const base64 = data.content;
+          const arrayBuffer = base64ToArrayBuffer(base64);
+          viewer.load(arrayBuffer);
+        })
+        .catch(err => {
+          console.error('加载失败：', err);
         });
     })
 
