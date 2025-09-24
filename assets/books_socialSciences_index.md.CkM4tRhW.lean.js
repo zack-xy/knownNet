815a977fe5a078303fb074b2435135ec3eac5930
zack-xy <zack-xy@users.noreply.github.com>
2025-09-24 17:40:38 +0000

    Deploying to gh-pages from @ zack-xy/knownNet@019b75c35abe9141a8db5b54d528447a85d110e6 🚀

---
+++ assets/books_socialSciences_index.md.CkM4tRhW.lean.js
@@ -0,0 +1,7 @@
+import{b as i,C as d}from"./chunks/theme.BquiOZ9z.js";/* empty css                     */import{_ as r,c,o as _,aG as p,G as a,w as o,a as n,j as t}from"./chunks/framework.Nr4pRSPI.js";const C=JSON.parse('{"title":"","description":"","frontmatter":{"showArticleMetadata":false,"editLink":false,"lastUpdated":false,"showComment":false},"headers":[],"relativePath":"books/socialSciences/index.md","filePath":"books/socialSciences/index.md"}'),u={name:"books/socialSciences/index.md"};function m(f,e,x,b,k,v){const s=i,l=d;return _(),c("div",null,[e[4]||(e[4]=p("",3)),a(s,null,{default:o(()=>[...e[0]||(e[0]=[n("没看完或者没有笔记的书",-1)])]),_:1}),a(l,{title:"《脑袋决定钱袋》"},{default:o(()=>[...e[1]||(e[1]=[t("div",null,[n(" 读了19页，鸡汤文学鉴定完毕，读完了也是那么回事，不读了 （看来是我想发财的心情非常急迫了，什么时候下载的这本书都不知道） "),t("pre",null,[t("code",null,`--------- 2025/04/03 11:32
+`)])],-1)])]),_:1}),a(l,{title:"《一想到还有95%的问题留给人类我就放心了》"},{default:o(()=>[...e[2]||(e[2]=[t("div",null,[n(" 这个，怎么说呢，笔记不好记啊，基本上都是物理内容 "),t("pre",null,[t("code",null,`记录下进度吧   
+
+25% 2025年4月10日 
+
+2025年04月14日 基本读完，剩了三章，不读了，科普读物，就这样了
+`)])],-1)])]),_:1}),a(l,{title:"《手机摄影，凭什么比相机还酷》"},{default:o(()=>[...e[3]||(e[3]=[t("div",null," 只要不是编程写代码，什么我都愿意看一看😓，可能是因为写代码已经与工作绑定了吧。 只要一想起来写代码，就是要工作，一要工作，就比较痛苦。也许还是没有找到工作的乐趣吧。 ",-1)])]),_:1})])}const N=r(u,[["render",m]]);export{C as __pageData,N as default};
