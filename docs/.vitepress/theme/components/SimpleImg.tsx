651e0d26533ede3192a0f8cca1a819890de5aa28
zack zheng <zack_zhengxiyun@163.com>
2025-09-28 12:45:05 +0800

    docs: update

--- docs/.vitepress/theme/components/SimpleImg.tsx
+++ docs/.vitepress/theme/components/SimpleImg.tsx
@@ -1,22 +1,39 @@
 export default defineComponent({
   props: {
     src: String,
+    lazy: Boolean
   },
   setup(props) {
+    const { src, lazy } = props
+    const title = src!.slice(55).split(".")[0]
+    const visible = ref<boolean>(lazy ? false : true);
+    const setVisible = (value: boolean): void => {
+      visible.value = value;
+      console.log("visible", visible.value);
+    };
     return () => {
-      const { src } = props
       return (
-        <div class="custom-simple-img">
-          <a-image
-            width="100%"
-            height="100%"
-            loading="lazy"
-            fallback="https://gitee.com/zackzhengxy/picGallery/raw/main/imgs/not_found.svg"
-            preview={{
-              maskClassName: 'simple-img',
-            }}
-            src={src?.startsWith('http') ? src : `/knownNet/svgs/${src}`} />
-        </div >
+        <>
+          <div class="custom-simple-img">
+            {
+              lazy && <a-button type="link" size="large" onClick={() => setVisible(true)}>{title}</a-button>
+            }
+            <a-image
+              style={lazy ? { display: 'none' } : {}}
+              width="100%"
+              height="100%"
+              loading="lazy"
+              fallback="https://gitee.com/zackzhengxy/picGallery/raw/main/imgs/not_found.svg"
+              preview={{
+                maskClassName: 'simple-img',
+                ...(lazy ? {
+                  visible: visible.value,
+                  onVisibleChange: setVisible,
+                } : {})
+              }}
+              src={src?.startsWith('http') ? src : `/knownNet/svgs/${src}`} />
+          </div >
+        </>
       )
     }
   },
--- docs/others/feature/React特性/useRecuder及其他.md
+++ docs/others/feature/React特性/useRecuder及其他.md
@@ -12,6 +12,8 @@ tags:
 
 useState 的替代方案。它接收一个形如 (state, action) => newState 的 reducer，并返回当前的 state 以及与其配套的 dispatch 方法
 
+  <simple-img src="https://gitee.com/zackzhengxy/picGallery/raw/main/imgs/useReducer是什么.png" lazy />
+
 #### startTransition
 
 利用startTransition这个方法来实现不紧急的任务操作,这会告诉 React 它们可以被中断执行。这样可以把紧急的任务先更新，不紧急的任务后更新。 
